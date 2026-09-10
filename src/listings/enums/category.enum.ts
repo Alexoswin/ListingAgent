62cc882fe04d3a8a59783f7f29dddb7d6371aaa6
Alexoswin <oswin.alex@mple.ai>
2026-09-10 17:05:32 +0530

    updated cat enum

--- src/listings/enums/category.enum.ts
+++ src/listings/enums/category.enum.ts
@@ -7,13 +7,9 @@ export enum Category {
 export enum Subcategory {
   Laptops = 'Laptops',
   Phones = 'Phones',
-  Headphones = 'Headphones',
-  OtherTech = 'Other Tech',
   Sofas = 'Sofas',
   Beds = 'Beds',
-  Wardrobes = 'Wardrobes',
-  CoffeeTables = 'Coffee Tables',
-  HomeDecor = 'Home Decor',
+
   Fridges = 'Fridges',
   AirCoolers = 'Air Coolers',
 }
@@ -22,15 +18,12 @@ export const CATEGORY_SUBCATEGORIES: Record<Category, Subcategory[]> = {
   [Category.Electronics]: [
     Subcategory.Laptops,
     Subcategory.Phones,
-    Subcategory.Headphones,
-    Subcategory.OtherTech,
+
   ],
   [Category.Furniture]: [
     Subcategory.Sofas,
     Subcategory.Beds,
-    Subcategory.Wardrobes,
-    Subcategory.CoffeeTables,
-    Subcategory.HomeDecor,
+ 
   ],
   [Category.HomeAppliances]: [Subcategory.Fridges, Subcategory.AirCoolers],
 };
